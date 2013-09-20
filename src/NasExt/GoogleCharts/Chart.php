a60ddc39f8c9dc97453ad44122da9964d0bf8f49
duskohu <duskohu@gmail.com>
2013-09-20 16:39:22 +0200

    Fixed Copyright

--- src/NasExt/GoogleCharts/Chart.php
+++ src/NasExt/GoogleCharts/Chart.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the NasExt extensions of Nette Framework
  *
- * Copyright (c) 20013 Dusan Hudak (http://dusan-hudak.com)
+ * Copyright (c) 2013 Dusan Hudak (http://dusan-hudak.com)
  *
  * For the full copyright and license information, please view
  * the file license.txt that was distributed with this source code.
